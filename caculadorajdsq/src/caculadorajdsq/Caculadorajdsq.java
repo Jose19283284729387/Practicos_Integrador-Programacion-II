805cf161a1b2ee252faa2ab3ff7a81ab7d8bcbd6
Jose19283284729387 <129287691+Jose19283284729387@users.noreply.github.com>
2025-11-27 15:19:00 -0300

    Esta es una calculdora basica, en lenguaje java
    link de tuto: https://www.youtube.com/watch?v=3VJ9_PncdHg

---
+++ caculadorajdsq/src/caculadorajdsq/Caculadorajdsq.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caculadorajdsq;
+
+/**
+ *
+ * @author LABO B CEN
+ */
+public class Caculadorajdsq {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
