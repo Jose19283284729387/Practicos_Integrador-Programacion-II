8287cd17ecd5457f1a7eb277a0b637c5b6731b51
Jose19283284729387 <129287691+Jose19283284729387@users.noreply.github.com>
2025-11-27 15:20:57 -0300

    Esta es una ajenda electronica , en lenguaje java
    link de tuto: https://www.youtube.com/watch?v=8ahuIJEOxf4&t=2374s

---
+++ ageendada_eleectronicaa/src/ageendada_eleectronicaa/Ageendada_eleectronicaa.java
@@ -0,0 +1,15 @@
+
+package ageendada_eleectronicaa;
+
+import Priniccpaal.principal;
+public class Ageendada_eleectronicaa {
+
+ 
+    public static void main(String[] args) {
+        principal princ = new principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+    
+}
